9188c349ae99f2bf224f70acc0ef5ce6acf3bdac
Alikhan <lifergamerus@gmail.com>
2022-06-26 16:54:14 +0600

    lesson#19

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 "use strict";
 
-let a = 5;
-let b = a + 3;
-console.log("Ty eblan " + b);
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "1");
+
+const personalMovieDB = {
+	count: +numberOfFilms,
+	movies: {
+
+	},
+	actors:{
+
+	},
+	genres:[],
+	privat: false
+};
+for(let i = 0; i < 2;i++){
+	let nameOfFilm = prompt("Один из последних просмотренных фильмов?","");
+	let rateOfFilm = +prompt("На сколько его оцените?","");
+	// personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?","")] = prompt("На сколько его оцените?","");
+	personalMovieDB.movies[nameOfFilm] = rateOfFilm;
+}
+
+console.log(personalMovieDB['movies']);
+
